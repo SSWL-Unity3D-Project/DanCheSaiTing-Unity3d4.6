498462e34da5328276f257b161533ba00773a8a9
syq <shenyongqiang2008@126.com>
2017-12-27 09:44:38 +0800

    添加ngui的UITexture动画控制逻辑

---
+++ Gui/UITextureAnimation.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(UITexture))]
+public class UITextureAnimation : MonoBehaviour
+{
+    public Texture mTexture;
+    UITexture mUITexture;
+    void Start()
+    {
+        mUITexture = GetComponent<UITexture>();
+    }
+
+    void Update()
+    {
+        if (mUITexture.mainTexture != mTexture)
+        {
+            mUITexture.mainTexture = mTexture;
+        }
+    }
+
+    /// <summary>
+    /// 动画事件回调.
+    /// </summary>
+    public void OnAnimationTrigger(int index)
+    {
+        //Debug.Log("OnAnimationTrigger -> index is " + index);
+        //广播消息.
+        SendMessage("OnAnimationEnvent", index);
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmosSelected()
+    {
+        if (mUITexture == null)
+        {
+            mUITexture = GetComponent<UITexture>();
+        }
+
+        if (mUITexture.mainTexture != mTexture)
+        {
+            mUITexture.mainTexture = mTexture;
+        }
+    }
+#endif
+}
