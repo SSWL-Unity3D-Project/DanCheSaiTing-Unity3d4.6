d46e0b863460f2c66316f733ab34eb0728bc3a8d
syq <shenyongqiang2008@126.com>
2018-03-21 17:25:55 +0800

    控制动画播放速度

---
+++ Gui/UIAniTimeScale.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class UIAniTimeScale : MonoBehaviour
+{
+    public Animator mAnimator;
+    void Update ()
+    {
+        float sp = -Time.timeScale + 2f;
+        sp = Mathf.Clamp(sp, 0.5f, 1.5f);
+        mAnimator.speed = sp;
+    }
+}
