773bd16ad5b821d35ba1b18c7b1b91c04f2ee731
syq <shenyongqiang2008@126.com>
2018-03-22 09:23:32 +0800

    优化

--- Gui/UIAniTimeScale.cs
+++ Gui/UIAniTimeScale.cs
@@ -5,8 +5,11 @@ public class UIAniTimeScale : MonoBehaviour
     public Animator mAnimator;
     void Update ()
     {
-        float sp = -Time.timeScale + 2f;
-        sp = Mathf.Clamp(sp, 0.5f, 1.5f);
-        mAnimator.speed = sp;
+        if (mAnimator != null)
+        {
+            float sp = -Time.timeScale + 2f;
+            sp = Mathf.Clamp(sp, 0.5f, 1.5f);
+            mAnimator.speed = sp;
+        }
     }
 }
