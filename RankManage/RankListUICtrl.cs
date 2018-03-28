f7b3bef0d37a2189303cbd1712abc4f19e5026d0
syq <shenyongqiang2008@126.com>
2018-03-28 15:16:21 +0800

    修改排行界面逻辑

--- RankManage/RankListUICtrl.cs
+++ RankManage/RankListUICtrl.cs
@@ -26,49 +26,58 @@ public class RankListUICtrl : MonoBehaviour
         for (int i = 0; i < 4; i++)
 		{
 			mRankDtUIArray[i].gameObject.SetActive(true);
-            rankDt = SSGameCtrl.GetInstance().mSSGameRoot.mSSGameDataManage.mGameData.RankDtManage.RankDtList[i];
-            if (rankDt.IsPlayerData)
+            if (SSGameCtrl.GetInstance().mSSGameRoot.mSSGameDataManage.mGameData.RankDtManage.RankDtList.Count > i)
             {
-                mRankDtUIArray[i].ShowPlayerRankBeiJingUI(mPlayerBJTexture);
-                mRankDtUIArray[i].transform.localScale += new Vector3(0.1f, 0.1f, 0f);
+                rankDt = SSGameCtrl.GetInstance().mSSGameRoot.mSSGameDataManage.mGameData.RankDtManage.RankDtList[i];
+                if (rankDt.IsPlayerData)
+                {
+                    mRankDtUIArray[i].ShowPlayerRankBeiJingUI(mPlayerBJTexture);
+                    mRankDtUIArray[i].transform.localScale += new Vector3(0.1f, 0.1f, 0f);
 #if UNITY_EDITOR
-                Debug.Log("ShowRankListUI -> PlayerTimeUsed " + rankDt.TimeUsedVal.ToString("f2")
-                    + ", disMove " + rankDt.DisMoveVal.ToString("f2"));
+                    Debug.Log("ShowRankListUI -> PlayerTimeUsed " + rankDt.TimeUsedVal.ToString("f2")
+                        + ", disMove " + rankDt.DisMoveVal.ToString("f2"));
 #endif
-            }
+                }
 
-//#if UNITY_EDITOR
-            Debug.Log("ShowRankListUI -> index " + i + ", RankType " + rankDt.RankType);
-//#endif
+                //#if UNITY_EDITOR
+                Debug.Log("ShowRankListUI -> index " + i + ", RankType " + rankDt.RankType);
+                //#endif
 
-            int disVal = (int)(pathDis - rankDt.DisMoveVal);
-            float wanChengDu = rankDt.DisMoveVal / pathDis;
-            if (rankDt.IsMoveToFinishPoint)
-            {
-                wanChengDu = 1f;
+                int disVal = (int)(pathDis - rankDt.DisMoveVal);
+                float wanChengDu = rankDt.DisMoveVal / pathDis;
+                if (rankDt.IsMoveToFinishPoint)
+                {
+                    wanChengDu = 1f;
+                }
+                else
+                {
+                    wanChengDu = wanChengDu > 1f ? 1f : wanChengDu;
+                }
+                mRankDtUIArray[i].ShowWanChengDuInfo((int)(wanChengDu * 100f));
+
+                if (rankDt.IsMoveToFinishPoint || wanChengDu >= 1f)
+                {
+                    mRankDtUIArray[i].ShowTimeUsedVal((int)rankDt.TimeUsedVal);
+                }
+                else
+                {
+                    mRankDtUIArray[i].HiddenTimeUI();
+                }
+
+                if (rankDt.IsMoveToFinishPoint || wanChengDu >= 1f)
+                {
+                    mRankDtUIArray[i].ShowShengYuDisMoveInfo(0);
+                }
+                else
+                {
+                    mRankDtUIArray[i].ShowShengYuDisMoveInfo(disVal < 0 ? 0 : disVal);
+                }
             }
             else
             {
-                wanChengDu = wanChengDu > 1f ? 1f : wanChengDu;
-            }
-            mRankDtUIArray[i].ShowWanChengDuInfo((int)(wanChengDu * 100f));
-            
-            if (rankDt.IsMoveToFinishPoint || wanChengDu >= 1f)
-            {
-                mRankDtUIArray[i].ShowTimeUsedVal((int)rankDt.TimeUsedVal);
-            }
-            else
-            {
-                mRankDtUIArray[i].HiddenTimeUI();
-            }
-
-            if (rankDt.IsMoveToFinishPoint || wanChengDu >= 1f)
-            {
-                mRankDtUIArray[i].ShowShengYuDisMoveInfo(0);
-            }
-            else
-            {
-                mRankDtUIArray[i].ShowShengYuDisMoveInfo(disVal < 0 ? 0 : disVal);
+                Debug.Log("rankDataList was wrong! i == " + i);
+                mRankDtUIArray[i].ShowWanChengDuInfo(2);
+                mRankDtUIArray[i].ShowShengYuDisMoveInfo((int)pathDis - 60);
             }
         }
         gameObject.SetActive(true);
